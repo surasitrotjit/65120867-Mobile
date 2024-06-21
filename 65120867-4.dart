e96b03b31831c6e8bb8dc6ee2c8d8193619506a4
surasitrotjit <140129098+surasitrotjit@users.noreply.github.com>
2024-06-21 11:58:21 +0700

    Create 65120867-4.dart

---
+++ 65120867-4.dart
@@ -0,0 +1,14 @@
+int factorial(int n) {
+  if (n == 0) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+
+void main() {
+  int n = 5;
+
+  int result = factorial(n);
+  print("The factorial of $n is: $result");
+}
